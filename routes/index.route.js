a22a0105df0ec43f86368a32c5bc50259f9a637f
Andrey Shelkovnikov <shelkov09@gmail.com>
2018-11-06 18:36:21 +0200

    add nodemon

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,6 +1,9 @@
 module.exports = function(app) {
   app.route('/')
     .get(function(req, res) {
-      res.render('index', { title: 'Express' });
+      res.render('index', {
+        title: 'Request Trap',
+        author: 'Shelkovnikov Andrey'
+      });
     });
 };
