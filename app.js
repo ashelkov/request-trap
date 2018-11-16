5d5d175fdba8d02bca34f657df6dcb6cbf126a33
Andrey Shelkovnikov <shelkov09@gmail.com>
2018-11-16 18:45:59 +0200

    add .env

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var dotenv = require('dotenv');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var routes = require('./routes');
 
 var app = express();
 
+// apply .env variables 
+dotenv.config({
+  path: path.join(__dirname, 'config/.env')
+});
+const { NODE_ENV } = process.env;
+
 // init mongose models
 require('./models/request.model');
 
 // mongoose instance connection
-const mongodbURL = 'mongodb://ashe:andrey1986@ds153763.mlab.com:53763/request-trap';
+const mongodbURL = process.env[`DB_${NODE_ENV}`.toUpperCase()];
 const opts = {
   useCreateIndex: true,
   useNewUrlParser: true,
